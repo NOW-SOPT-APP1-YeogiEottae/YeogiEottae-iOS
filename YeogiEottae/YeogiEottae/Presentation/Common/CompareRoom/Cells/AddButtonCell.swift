9ac7fb1397ad70bdcfed8cdf88739b0c422ba7cb
kim-seonwoo <sw204011@gmail.com>
2024-05-17 12:55:11 +0900

    [Feat/#6] AddButtonCell 구현

--- YeogiEottae/YeogiEottae/Presentation/Common/CompareRoom/Cells/AddButtonCell.swift
+++ YeogiEottae/YeogiEottae/Presentation/Common/CompareRoom/Cells/AddButtonCell.swift
@@ -5,4 +5,39 @@
 //  Created by Seonwoo Kim on 5/16/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class AddButtonCell: UITableViewCell {
+
+    private let addButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("+", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        return button
+    }()
+    
+    var addButtonAction: (() -> Void)?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        selectionStyle = .none
+        contentView.addSubview(addButton)
+        addButton.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20))
+        }
+        addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func addButtonTapped() {
+        addButtonAction?()
+    }
+}
